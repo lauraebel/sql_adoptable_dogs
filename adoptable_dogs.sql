633a945eaccd89dc81ca3380a59a27fa267e41c2
Laura Ebel <laura.ebel7@gmail.com>
2023-09-12 16:23:31 -0400

    importing data from csv

--- adoptable_dogs.sql
+++ adoptable_dogs.sql
@@ -6,8 +6,8 @@ dog_id INT,
 name varchar(255),
 breed_primary varchar(255),
 breed_secondary varchar(255),
-mixed_breed boolean,
-breed_unknown boolean,
+mixed_breed varchar(30),
+breed_unknown varchar(30),
 color_primary varchar(255),
 color_secondary varchar(255),
 color_tertiary varchar(255),
@@ -15,12 +15,21 @@ age varchar(30),
 sex varchar(30),
 size varchar(30),
 coat varchar(30),
-fixed boolean,
-house_trained boolean,
-special_needs boolean,
-shots_uptodate boolean,
-good_with_kids boolean,
-good_with_dogs boolean,
-good_with_cats boolean,
-description varchar(255)
+fixed varchar(30),
+house_trained varchar(30),
+special_needs varchar(30),
+shots_uptodate varchar(30),
+good_with_kids varchar(30),
+good_with_dogs varchar(30),
+good_with_cats varchar(30)
 );
+
+-- DROP TABLE adoptable_dogs.dogs;
+
+-- Importing the csv file with the information for the dogs table
+-- LOAD DATA LOCAL INFILE 'c:/Users/lauraebel/Portfolio/adoptableDogs/dogs.csv'
+-- INTO TABLE adoptable_dogs.dogs
+-- FIELDS TERMINATED BY ',' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS; 
