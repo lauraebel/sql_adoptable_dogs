daddb6f8e66609b3b5259c983b61eb3b056e1b02
Laura Ebel <laura.ebel7@gmail.com>
2023-09-12 16:56:29 -0400

    proving most homeless dogs are mixed breed

--- adoptable_dogs.sql
+++ adoptable_dogs.sql
@@ -1,4 +1,10 @@
-SELECT * FROM adoptable_dogs.dogs;
+-- SELECT * FROM adoptable_dogs.dogs;
 
--- Changing the column name for dog_id due to stange formatting when imported
+-- Counting the total number of individual dogs in the table (1598)
+SELECT COUNT(dog_id) AS total_dogs FROM adoptable_dogs.dogs;
 
+-- Counting the number of purebred dogs in the table (438)
+SELECT COUNT(dog_id) AS purebred_dogs FROM adoptable_dogs.dogs WHERE mixed_breed = 'No';
+
+-- Counting the number of mixed breed dogs in the table (1159) to conclude most homesless dogs are mixed breed
+SELECT COUNT(dog_id) AS mixed_dogs FROM adoptable_dogs.dogs WHERE mixed_breed = 'Yes';
